dec7a516434b33997450c1b79a41e385f7f74a97
Stefan Countryman <stefan.countryman@gmail.com>
2020-06-26 17:58:54 -0400

    add profilegraph.py

---
+++ profilegraph.py
@@ -0,0 +1,31 @@
+import os
+import re
+from subprocess import check_output
+from tempfile import NamedTemporaryFile
+from pathlib import Path
+from IPython.display import HTML
+from IPython.core.magic import register_line_magic
+import cProfile
+import gprof2dot
+
+
+def profilegraph(line):
+    tmp = NamedTemporaryFile(suffix='.pstats', mode='w', delete=False)
+    try:
+        tmp.close()
+        cProfile.run(statement=line, filename=tmp.name)
+        dtmp = re.sub(".pstats$", ".dot", tmp.name)
+        try:
+            gprof2dot.main(['-f', 'pstats', '-o', dtmp, tmp.name])
+            return HTML(check_output(['dot', '-Tsvg'],
+                                     input=Path(dtmp).read_bytes()).decode())
+        finally:
+            if os.path.isfile(dtmp):
+                os.unlink(dtmp)
+    finally:
+        if os.path.isfile(tmp.name):
+            os.unlink(tmp.name)
+
+
+def load_ipython_extension(ipython):
+    register_line_magic(profilegraph)
